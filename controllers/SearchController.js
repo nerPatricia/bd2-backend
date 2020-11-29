0431dddb810b587770cce6119432d89142dcd880
bruna-braga <bruna.braga@estantemagica.com.br>
2020-11-29 03:49:25 -0300

    fix search

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -8,42 +8,60 @@ module.exports.MongoSearch = async (req, res) => {
     const limit = req.query.limit || 30;
     const name = req.query.name || '';
     const collection = req.query.collection || '';
-    const usd_max=req.query.usd_max || 0;
-    const usd_foil_max=req.query.usd_foil_max || 0;
-    const eur_max=req.query.eur_max || 0;
-    const eur_foil_max=req.query.eur_foil_max || 0;
-    const usd_min=req.query.usd_min || 0;
-    const usd_foil_min=req.query.usd_foil_min || 0;
-    const eur_min=req.query.eur_min || 0;
-    const eur_foil_min=req.query.eur_foil_min || 0;
-    const mana_cost = req.query.mana_cost || '';
+    const usd_max=req.query.usd_max || null;
+    const usd_foil_max=req.query.usd_foil_max || null;
+    const eur_max=req.query.eur_max || null;
+    const eur_foil_max=req.query.eur_foil_max || null;
+    const usd_min=req.query.usd_min || null;
+    const usd_foil_min=req.query.usd_foil_min || null;
+    const eur_min=req.query.eur_min || null;
+    const eur_foil_min=req.query.eur_foil_min || null;
+    const cmc = req.query.cmc || '';
     const rarity = req.query.rarity || '';
-    const format = req.query.format || '';
+    const legalities = req.query.legalities || '';
     const releaseDate_max = req.query.releaseDate_max || '';
     const releaseDate_min = req.query.releaseDate_min || '';
     const power = req.query.power || '';
     const toughness = req.query.toughness || '';
-    const keyword = req.query.keyword || '';
-    const cards = await Card.paginate({$or:[
-        {name},
-        {collection},
-        {"prices.usd":{"$lt": usd_max}},
-        {"prices.usd_foil":{"$lt": usd_foil_max}},
-        {"prices.eur":{"$lt": eur_max}},
-        {"prices.eur_foil":{"$lt": eur_foil_max}},
-        {"prices.usd":{"$gt": usd_min}},
-        {"prices.usd_foil":{"$gt": usd_foil_min}},
-        {"prices.eur":{"$gt": eur_min}},
-        {"prices.eur_foil":{"$gt": eur_foil_min}},
-        {mana_cost},
-        {rarity},
-        {format},
-        {"releaseDate": {"$lt": releaseDate_max}},
-        {"releaseDate": {"$gt": releaseDate_min}},
-        {power},
-        {toughness},
-        {keyword},
-    ]}, { page, limit });
+    const keywords = req.query.keywords || '';
+    const colors = req.query.colors || '';
+    let query = {};
+    Object.keys(req.query).forEach(field =>{
+        //TODO
+        if(field == usd_max){
+            query["prices.usd"]={"$lt": usd_max};
+        }
+        if(field == usd_max){}
+        // ...
+        else{
+            query[field]=req.query[field];
+        }
+
+    })
+    const cards = await Card.paginate(
+    //     {$or:[
+    //     {name},
+    //     {collection},
+    //     {"prices.usd":{"$lt": usd_max}},
+    //     {"prices.usd_foil":{"$lt": usd_foil_max}},
+    //     {"prices.eur":{"$lt": eur_max}},
+    //     {"prices.eur_foil":{"$lt": eur_foil_max}},
+    //     {"prices.usd":{"$gt": usd_min}},
+    //     {"prices.usd_foil":{"$gt": usd_foil_min}},
+    //     {"prices.eur":{"$gt": eur_min}},
+    //     {"prices.eur_foil":{"$gt": eur_foil_min}},
+    //     {cmc},
+    //     {rarity},
+    //     {format},
+    //     {"releaseDate": {"$lt": releaseDate_max}},
+    //     {"releaseDate": {"$gt": releaseDate_min}},
+    //     {power},
+    //     {toughness},
+    //     {keywords},
+    //     {colors}
+    // ]}
+    query, { page, limit, select:'name'});
+    console.log("QUERY: ", query);
     return res.json(cards);
     
 };
